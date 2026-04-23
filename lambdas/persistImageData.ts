79a0b24524017ce0852472b03bac594675d4729d
amycomynshaugh03 <comynsamy03@gmail.com>
2026-04-23 14:27:47 +0100

    Filter Topic Subscribers

--- lambdas/persistImageData.ts
+++ lambdas/persistImageData.ts
@@ -18,8 +18,19 @@ export const handler: SQSHandler = async (event) => {
     if (snsMessage.Records) {
       for (const s3Message of snsMessage.Records) {
         const s3e = s3Message.s3;
-        const srcBucket = s3e.bucket.name;
+        // Object key may have spaces or unicode non-ASCII characters.
         const srcKey = decodeURIComponent(s3e.object.key.replace(/\+/g, " "));
+        // Infer the image type from the file suffix.
+        const typeMatch = srcKey.match(/\.([^.]*)$/);
+        if (!typeMatch) {
+          console.log("Could not determine the image type.");
+          throw new Error("Could not determine the image type.");
+        }
+        // Check that the image type is supported
+        const imageType = typeMatch[1].toLowerCase();
+        if (imageType != "jpeg" && imageType != "png") {
+          throw new Error(`Unsupported image type: ${imageType}`);
+        }
         try {
           await ddbDocClient.send(
             new PutCommand({
